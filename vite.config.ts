a5abb4e52a517a6d8d0b80cbb6479be69e3299d2
batdimoiprint <johnkennyreyes21@gmail.com>
2026-07-22 04:42:18 +0800

    feat(vite): add /api proxy rules to Vite server config

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,11 @@ export default defineConfig({
   ],
   server: {
     port: 8081,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL || "https://func-msc-qcu-backend-dev.azurewebsites.net",
+        changeOrigin: true,
+      },
+    },
   },
 });
